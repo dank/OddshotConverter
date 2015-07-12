6686ad6490a2d2a1455aef36f6dd6e98851e23d1
nicememe <rmadanny@hotmail.com>
2015-07-12 11:19:46 -0400

    Fixed "pending" shots

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var rawjs = require('raw.js');
-var reddit = new rawjs('Oddshot Converter by /u/iEyepawd (rel-1.1)');
+var reddit = new rawjs('Oddshot Converter by /u/iEyepawd (rel-1.2)');
 var models = require('./src/sequelize');
 var youtube = require('./src/youtube');
 var fs = require('fs');
@@ -30,45 +30,7 @@ var fetch = function(subreddit) {
                models.posts.count({where: {postUrl: data.data.url.toLowerCase()}}).then(function(c) {
                   if(c === 0) {
                      models.posts.create({postId: data.data.id, postUrl: data.data.url.toLowerCase(), mirror: 'pending', commentId: 'pending'}).then(function(post) {
-                        request(data.data.url, function (err , res, body) {
-                           if(err) {
-                              error('Request error. Error: ' + err);
-                           } else if(res.statusCode != 200) {
-                              error('Request error. Error: Status Code ' + res.statusCode);
-                           } else {
-                              var url = body.match('source src=(?:"|\')(.*?)(?:"|\') type=(?:"|\')video/mp4(?:"|\')')[1];
-                              var dir = './tmp/' + data.data.id + '.mp4';
-
-                              if (!fs.existsSync('./tmp/')) { // TODO: Apparently this is bad practice (?)
-                                 fs.mkdirSync('./tmp/');
-                              }
-
-                              request(url).pipe(fs.createWriteStream(dir)).on('close', function() {
-                                 youtube.upload(data.data.title, 'I am a bot. Please report any bugs to me, my contact info can be found below.\nOriginal video: ' + data.data.url + '\n\nGitHub: https://github.com/nicememe/oddshotconverter\nContact: https://keybase.io/gay\nFAQ: https://www.reddit.com/r/OddshotBot/wiki/faq', dir, function (err, up) {
-                                    if (err) {
-                                       error('Upload error. Error: ' + err);
-                                    } else {
-                                       var ytUrl = 'https://youtu.be/' + up.id;
-                                       post.updateAttributes({mirror: ytUrl});
-
-                                       reddit.comment(data.data.name, '[YouTube Mirror](' + ytUrl + ')\n****\n^I ^am ^a ^bot. ^Feel ^free ^to ^send ^me ^any ^bugs/suggestions/comments!  \n^[github](https://github.com/nicememe/oddshotconverter) ^- ^[contact](https://keybase.io/gay) ^- ^[faq](https://www.reddit.com/r/OddshotBot/wiki/faq)', function(err, comment) {
-                                          if(err) {
-                                             error('Failed to comment. Error: ' + err);
-                                          } else {
-                                             post.updateAttributes({commentId: comment.data.id});
-
-                                             fs.unlink(dir, function(err) {
-                                                if(err) {
-                                                   error('Error removing file. Error: ' + err);
-                                                }
-                                             })
-                                          }
-                                       });
-                                    }
-                                 });
-                              });
-                           }
-                        });
+                        fetchOnce(data, post);
                      });
                   }
                });
@@ -77,6 +39,52 @@ var fetch = function(subreddit) {
       }
    });
 };
+
+var fetchOnce = function(data, post) {
+   request(data.data.url, function (err , res, body) {
+      if(err) {
+         error('Request error. Error: ' + err);
+      } else if(res.statusCode != 200) {
+         error('Request error. Error: Status Code ' + res.statusCode);
+      } else {
+         if(body.indexOf('Shot is pending...') < 0 && body.indexOf('Shot processing...') < 0) {
+            var url = body.match('source src=(?:"|\')(.*?)(?:"|\') type=(?:"|\')video/mp4(?:"|\')')[1];
+            var dir = './tmp/' + data.data.id + '.mp4';
+
+            if (!fs.existsSync('./tmp/')) { // TODO: Apparently this is bad practice (?)
+               fs.mkdirSync('./tmp/');
+            }
+
+            request(url).pipe(fs.createWriteStream(dir)).on('close', function () {
+               youtube.upload(data.data.title, 'I am a bot. Please report any bugs to me, my contact info can be found below.\nOriginal video: ' + data.data.url + '\n\nGitHub: https://github.com/nicememe/oddshotconverter\nContact: https://keybase.io/gay\nFAQ: https://www.reddit.com/r/OddshotBot/wiki/faq', dir, function (err, up) {
+                  if (err) {
+                     error('Upload error. Error: ' + err);
+                  } else {
+                     var ytUrl = 'https://youtu.be/' + up.id;
+                     post.updateAttributes({mirror: ytUrl});
+
+                     reddit.comment(data.data.name, '[YouTube Mirror](' + ytUrl + ')\n****\n^I ^am ^a ^bot. ^Feel ^free ^to ^send ^me ^any ^bugs/suggestions/comments!  \n^[github](https://github.com/nicememe/oddshotconverter) ^- ^[contact](https://keybase.io/gay) ^- ^[faq](https://www.reddit.com/r/OddshotBot/wiki/faq)', function (err, comment) {
+                        if (err) {
+                           error('Failed to comment. Error: ' + err);
+                        } else {
+                           post.updateAttributes({commentId: comment.data.id});
+
+                           fs.unlink(dir, function (err) {
+                              if (err) {
+                                 error('Error removing file. Error: ' + err);
+                              }
+                           })
+                        }
+                     });
+                  }
+               });
+            });
+         } else {
+            setTimeout(fetchOnce, 10000, data, post);
+         }
+      }
+   });
+}
 
 var error = function(log){
    models.errors.create({error: log}).then(function() {
